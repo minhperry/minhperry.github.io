39fedf6eca861bffd2c724481f256d36a8ae819b
Tuan Minh Phan <phan.minh@tu-dortmund.de>
2024-07-19 21:54:11 +0200

    i fucking hate angular git

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { Tabs2Module } from '@coreui/angular';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +13,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    Tabs2Module
   ],
   providers: [],
   bootstrap: [AppComponent]
