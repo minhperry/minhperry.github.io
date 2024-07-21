9094a5de2150b0a74cded39ddf9d583e2c7e17e3
Tuan Minh Phan <phan.minh@tu-dortmund.de>
2024-07-21 20:57:11 +0200

    delete old repo

--- old/script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-    const elements = document.querySelectorAll('.animate-fade-in');
-    elements.forEach((element) => {
-        element.style.opacity = 0;
-        element.style.transition = 'opacity 0.5s ease-in-out';
-        setTimeout(() => element.style.opacity = 1, 100);
-    });
-});
