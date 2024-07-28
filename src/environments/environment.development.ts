4146380ed25b8522371e848058f209b7383a3b6f
Tuan Minh Phan <phan.minh@tu-dortmund.de>
2024-07-29 01:01:59 +0200

    Add more backend stuff

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    prod: false,
+    endpoint: 'localhost:8787'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    prod: true,
+    endpoint: 'https://backend.minhpt2002.workers.dev/'
+};
