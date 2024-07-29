355731af3b3a79b4b108cbaadbd0c91f491ac8e7
Tuan Minh Phan <phan.minh@tu-dortmund.de>
2024-07-29 14:45:33 +0200

    fix: cors v3

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,10 +2,8 @@ import { AutoRouter, cors } from 'itty-router' // ~1kB
 import { getSocialData } from './data/social-data'
 
 const { preflight, corsify } = cors({
-  origin: ['http://localhost:8787', 'https://backend.minhperry.workers.dev'],
-  allowMethods: ['GET', 'OPTIONS'],
-  allowHeaders: ['Content-Type'],
-  maxAge: 86400
+  origin: '*',
+  allowMethods: '*',
 }) 
 const router = AutoRouter({
   before: [preflight],
