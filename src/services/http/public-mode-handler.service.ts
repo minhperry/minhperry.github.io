14e7c7393edddbf2debfdf47e29d18eba224410b
Tuan Minh Phan <phan.minh@tu-dortmund.de>
2024-07-30 16:35:02 +0200

    cache requests

--- src/services/http/public-mode-handler.service.ts
+++ src/services/http/public-mode-handler.service.ts
@@ -1,16 +1,39 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
+import { Observable, tap, timer, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PublicModeHandlerService {
   private api = environment.apiUrl + 'public'
+
+  publicMode: boolean | null = null;
+  private lastFetch = 0;
+  private cacheDuration = 1000 * 60 * 5;
   
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { 
+    timer(this.cacheDuration, this.cacheDuration).subscribe(() => {
+      this.fetch().subscribe();
+    })
+  }
 
   get() {
-    return this.http.get<boolean>(this.api);
+    const now = Date.now();
+    if (this.publicMode === null || now - this.lastFetch > this.cacheDuration) {
+      return of(this.publicMode)
+    }
+
+    return this.fetch();
+  }
+
+  private fetch(): Observable<boolean> {
+    return this.http.get<boolean>(this.api).pipe(
+      tap(mode => {
+        this.publicMode = mode;
+        this.lastFetch = Date.now();
+      })
+    )
   }
 }
