1701ef422c6046b96432370b2e7a70f5df95f42f
Tuan Minh Phan <phan.minh@tu-dortmund.de>
2024-08-14 16:37:22 +0200

    feat: landing site

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
-  styleUrl: './landing.component.css'
+  styleUrl: './landing.component.scss'
 })
-export class LandingComponent {
+export class LandingComponent implements OnInit{
+  private backgrounds: string[] = [
+    'https://www.bucuti.com/storage/app/media/screensavers/dining-desktop.jpg',
+    'https://www.bucuti.com/storage/app/media/screensavers/sunset1-desktop.jpg',
+    'https://i.pinimg.com/originals/b7/f3/78/b7f378175ffba39680f53419dfb8d40f.jpg',
+    'https://pics.freeartbackgrounds.com/Night_Beach_Restauran_Background-1007.jpg'
+  ];
 
+  ngOnInit(): void {
+      this.setrRandomBG()
+  }
+
+  setrRandomBG() {
+    const randomIndex = Math.floor(Math.random() * this.backgrounds.length);
+    const selectedBackground = this.backgrounds[randomIndex];
+    document.querySelector('.landing-container')!.setAttribute('style', `background-image: url(${selectedBackground});`);
+  }
 }
