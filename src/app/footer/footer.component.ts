9b188ec3ab4060b10064be8a758f85d06d23fbc9
Tuan Minh Phan <phan.minh@tu-dortmund.de>
2024-08-15 21:13:16 +0200

    fix: tba mobile support in footer.

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-
+  marqueeText = [
+    'Mobile Support TBA',
+    'Redesign in Progress',
+    'I will never support light mode'
+  ]
 }
