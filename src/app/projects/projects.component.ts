abec0ea022dd2a5d9f8e416b05d24752a009ead6
Tuan Minh Phan <phan.minh@tu-dortmund.de>
2024-11-17 18:28:10 +0100

    separate link and source in projects

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -3,7 +3,8 @@ import {Component, OnInit} from '@angular/core';
 interface Project {
     title: string;
     description: string;
-    link: string;
+    source: string
+    link?: string;
     since?: string;
 }
 
@@ -17,43 +18,43 @@ export class ProjectsComponent implements OnInit {
         {
             title: 'Module \'Web-Technologie-2\'',
             description: 'A Twitter-like platform with Angular frontend and Spring backend.',
-            link: 'https://github.com/minhperry/wt2-ss23-tudo',
+            source: 'https://github.com/minhperry/wt2-ss23-tudo',
             since: 'From April 2023 to July 2023'
         },
         {
             title: 'Skyhanni',
             description: 'Open source contribution to the Minecraft mod \'Skyhanni\' for the game Hypixel Skyblock.',
-            link: 'https://github.com/minhperry/SkyHanni/',
+            source: 'https://github.com/minhperry/SkyHanni/',
             since: 'From June 2024 until now'
         },
         {
             title: 'Portfolio',
             description: 'This portfolio website. Uses Angular and Cloudflare Pages.',
-            link: './',
+            source: 'https://github.com/minhperry/minhperry.github.io',
             since: 'From July 2024 until now'
         },
         {
             title: 'Skyblock Simulation',
-            description: 'A simulation of various game mechanics in Hypixel Skyblock.',
-            link: 'https://skyblock.minhperry.de/',
+            description: 'A simulation of various game mechanics in Hypixel Skyblock. Self-hosted on a VPS.',
+            source: 'https://github.com/minhperry/skyblock-simulator',
+            link: 'https://skyblock.minhperry.de',
             since: 'From July 2024 until now'
         },
         {
             title: 'Blog',
             description: 'My personal blog built using Hugo',
+            source: 'https://github.com/minhperry/my-blog',
             link: 'https://blog.minhperry.de/',
             since: 'From October 2024 until now'
-        },
-        {
-            title: 'Link Shortener',
-            description: 'My own personal link shortener with custom short-keys, based on Cloudflare KV.',
-            link: 'https://s.7278008.xyz/source',
-            since: 'From August 2024 until now'
         }
     ]
 
     ngOnInit(): void {
         this.projects = this.returnShuffled();
+    }
+
+    hasLink(project: Project): boolean {
+        return 'link' in project;
     }
 
     private returnShuffled(): Project[] {
