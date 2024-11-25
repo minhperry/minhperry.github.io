3571558bf810d40f7d4276a5300f6a76f0b59bc9
Tuan Minh Phan <phan.minh@tu-dortmund.de>
2024-11-25 23:47:01 +0100

    forgot animation for Material

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,6 +5,7 @@ import {provideHttpClient, withFetch} from "@angular/common/http";
 import {SsrCookieService} from "ngx-cookie-service-ssr";
 import {MAT_SNACK_BAR_DEFAULT_OPTIONS} from "@angular/material/snack-bar";
 import {routes} from "./app.routes";
+import {provideAnimations} from "@angular/platform-browser/animations";
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -12,6 +13,7 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
     provideHttpClient(withFetch()),
+    provideAnimations(),
     SsrCookieService,
     { provide: MAT_SNACK_BAR_DEFAULT_OPTIONS, useValue: { duration: 3000 } }
   ]
