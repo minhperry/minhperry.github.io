4317e88bfaa1979942f41cc3ce9d5e86a84baece
Tuan Minh Phan <phan.minh@tu-dortmund.de>
2024-12-16 11:14:40 +0100

    remove test (for now)

--- src/directives/lang/language.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { LanguageDirective } from './language.directive';
-
-describe('LanguageDirective', () => {
-  it('should create an instance', () => {
-    const directive = new LanguageDirective();
-    expect(directive).toBeTruthy();
-  });
-});
