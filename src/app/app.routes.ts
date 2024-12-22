d54765c9b2be93813aef92040373c9d78e0e914a
Tuan Minh Phan <phan.minh@tu-dortmund.de>
2024-12-22 15:41:55 +0100

    re-indent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,30 +5,30 @@ import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 import {LandingComponent} from './landing/landing.component';
 
 export const routes: Routes = [
-    {
-        path: '',
-        component: LandingComponent,
-        title: "Welcome to my page"
-    },
-    {
-        path: 'me',
-        component: AboutmeComponent,
-        title: "About me",
-        // canActivate: [hasRoleGuard], // TODO: Change this in CV
-    },
-    {
-        path: 'socials',
-        component: SocialComponent,
-        title: "My social medias",
-    },
-    {
-        path: '404',
-        component: PageNotFoundComponent,
-        title: "404 Not Found",
-    },
-    {
-        path: '**',
-        redirectTo: '/404',
-        title: "Ooopsie woospie you made a fucky wucky"
-    }
+  {
+    path: '',
+    component: LandingComponent,
+    title: "Welcome to my page"
+  },
+  {
+    path: 'me',
+    component: AboutmeComponent,
+    title: "About me",
+    // canActivate: [hasRoleGuard], // TODO: Change this in CV
+  },
+  {
+    path: 'socials',
+    component: SocialComponent,
+    title: "My social medias",
+  },
+  {
+    path: '404',
+    component: PageNotFoundComponent,
+    title: "404 Not Found",
+  },
+  {
+    path: '**',
+    redirectTo: '/404',
+    title: "Ooopsie woospie you made a fucky wucky"
+  }
 ]
