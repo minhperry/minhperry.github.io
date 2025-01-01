c86fecbd969161f3bb4706b7f145d7467b35be05
Tuan Minh Phan <phan.minh@tu-dortmund.de>
2025-01-01 15:59:46 +0100

    rewrite nav bar -> clock service is obsolete

--- src/app/navibar/navibar.component.ts
+++ src/app/navibar/navibar.component.ts
@@ -2,6 +2,7 @@ import {Component, Inject, OnInit, PLATFORM_ID} from '@angular/core';
 import {RouterLink, RouterLinkActive} from "@angular/router";
 import {environment} from "../../environments/environment";
 import {Utils} from "../../services/utils/utils.service";
+import {MatTooltip} from "@angular/material/tooltip";
 interface NavLink {
   path: string;
   label: string;
@@ -14,7 +15,8 @@ interface NavLink {
   styleUrl: './navibar.component.scss',
   imports: [
     RouterLink,
-    RouterLinkActive
+    RouterLinkActive,
+    MatTooltip
   ]
 })
 export class NavibarComponent implements OnInit{
@@ -29,23 +31,34 @@ export class NavibarComponent implements OnInit{
 
   version = environment.version
   time = '';
+  private colon = true;
 
   constructor(@Inject(PLATFORM_ID) private platform: object) {
-    this.updateDate();
+    this.updateDate(!this.colon);
   }
 
   ngOnInit() {
     Utils.doIfBrowser(this.platform, () => {
       setInterval(() => {
-        this.updateDate();
+        this.updateDate(!this.colon);
       }, 1000);
     })
   }
 
-  private updateDate() {
-    this.time = new Date().toLocaleString("de-DE", {
+  private updateDate(colon: boolean) {
+    let date = new Date().toLocaleString("de-DE", {
       day: '2-digit', month: '2-digit', year: 'numeric',
       hour: '2-digit', minute: '2-digit', second: '2-digit'
     })
+    date = date.replace(/,/g, '');
+    let date2 = date;
+    if (colon) {
+      date2 = date.replace(/:/, ' ');
+    } else {
+      let i = 0;
+      date2 = date.replace(/:/g, match => ++i === 2 ? ' ' : match);
+    }
+    this.time = date2;
+    this.colon = !this.colon;
   }
 }
