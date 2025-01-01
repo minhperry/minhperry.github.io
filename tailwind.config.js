41cf51c263895b8e4422026362257971e8c58ffc
Tuan Minh Phan <phan.minh@tu-dortmund.de>
2025-01-01 17:22:03 +0100

    extend 3xl breakpoint

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,16 @@ module.exports = {
         'sans': ["Source Sance 3", "sans-serif"]
       }
     },
+    container: {
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        '2xl': '1536px',
+        '3xl': '1742px',
+      }
+    }
   },
   plugins: [],
 }
