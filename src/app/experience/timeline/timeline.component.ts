fa8a5d1ce936de70c0f6a3efe3b15ec4f4cfdaf4
Tuan Minh Phan <phan.minh@tu-dortmund.de>
2025-01-05 17:40:19 +0100

    fix style timeline

--- src/app/experience/timeline/timeline.component.ts
+++ src/app/experience/timeline/timeline.component.ts
@@ -1,5 +1,6 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {TimelineEvent} from "../../../interfaces/date-entry";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'p-timeline',
@@ -7,9 +8,19 @@ import {TimelineEvent} from "../../../interfaces/date-entry";
   templateUrl: './timeline.component.html',
   styleUrl: './timeline.component.scss'
 })
-export class TimelineComponent {
-  @Input({required: true}) timeline!: TimelineEvent[];
+export class TimelineComponent implements OnInit{
+  @Input({required: true}) timelineDataFile!: string;
   @Input({required: true}) title = '';
+
+  timeline: TimelineEvent[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get<TimelineEvent[]>(this.timelineDataFile).subscribe(data => {
+      this.timeline = data;
+    });
+  }
 
   appendDate(event: TimelineEvent) {
     return `${event.start} ➜ ${event.end ?? 'present'}`;
