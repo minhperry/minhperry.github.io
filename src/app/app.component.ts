41f3c7a82542f0c50f43c2d3cb3f82614dbc42bb
Tuan Minh Phan <phan.minh@tu-dortmund.de>
2025-01-06 19:30:39 +0100

    add footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterOutlet} from '@angular/router';
+import {Router, RouterOutlet} from '@angular/router';
 import {NavibarComponent} from "./navibar/navibar.component";
+import {FooterComponent} from "./footer/footer.component";
 
 @Component({
     selector: 'p-root',
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [NavibarComponent, RouterOutlet]
+    imports: [NavibarComponent, RouterOutlet, FooterComponent]
 })
 export class AppComponent {
+    constructor(private router: Router) {}
+
+    shouldRenderFooter(): boolean {
+        const excluded = ['/']
+        return !excluded.includes(this.router.url);
+    }
 }
