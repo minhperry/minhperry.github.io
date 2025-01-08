94ed9d42aa75c5c0df7224299dada100ca52ad87
Tuan Minh Phan <phan.minh@tu-dortmund.de>
2025-01-08 23:24:50 +0100

    nah remove flowbite useless ahhh

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,20 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {Router, RouterOutlet} from '@angular/router';
 import {NavibarComponent} from "./navibar/navibar.component";
 import {FooterComponent} from "./footer/footer.component";
-import {initFlowbite} from "flowbite";
 
 @Component({
-    selector: 'p-root',
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss',
-    imports: [NavibarComponent, RouterOutlet, FooterComponent]
+  selector: 'p-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+  imports: [NavibarComponent, RouterOutlet, FooterComponent]
 })
-export class AppComponent implements OnInit{
-    constructor(private router: Router) {}
+export class AppComponent {
+  constructor(private router: Router) {
+  }
 
-    ngOnInit() {
-        initFlowbite();
-    }
-
-    shouldRenderFooter(): boolean {
-        const excluded = ['/']
-        return !excluded.includes(this.router.url);
-    }
+  shouldRenderFooter(): boolean {
+    const excluded = ['/']
+    return !excluded.includes(this.router.url);
+  }
 }
