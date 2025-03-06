dfa629247a87993e5754f90b54124aeeed7bcdc3
Tuan Minh Phan <phan.minh@tu-dortmund.de>
2025-03-06 21:35:58 +0100

    save to localstorage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import {Router, RouterOutlet} from '@angular/router';
 import {NavibarComponent} from "./navibar/navibar.component";
 import {FooterComponent} from "./footer/footer.component";
 import {TranslateService} from "@ngx-translate/core"
+import {LocalStorageService} from '../services/local-storage/local-storage.service';
 
 @Component({
   selector: 'p-root',
@@ -11,10 +12,10 @@ import {TranslateService} from "@ngx-translate/core"
   imports: [NavibarComponent, RouterOutlet, FooterComponent]
 })
 export class AppComponent {
-  constructor(private router: Router, private trans: TranslateService) {
+  constructor(private router: Router, private trans: TranslateService, private ls: LocalStorageService) {
     this.trans.addLangs(['en', 'de', 'vi'])
     this.trans.setDefaultLang('en')
-    this.trans.use('en')
+    this.trans.use(this.ls.getItem('lang') || 'en')
   }
 
   shouldRenderFooter(): boolean {
--- src/app/language-picker/language-picker.component.ts
+++ src/app/language-picker/language-picker.component.ts
@@ -2,6 +2,7 @@ import {Component, Input} from '@angular/core';
 import {TranslateService} from '@ngx-translate/core';
 import {SelectButton, SelectButtonOptionClickEvent} from 'primeng/selectbutton';
 import {FormsModule} from '@angular/forms';
+import {LocalStorageService} from '../../services/local-storage/local-storage.service';
 
 export interface LanguageEntry {
   text: string,
@@ -23,13 +24,16 @@ export class LanguagePickerComponent {
 
   @Input({required: true}) panelSize: 'small' | 'medium' | 'large' = 'medium'
 
-  constructor(protected trans: TranslateService) {
-    this.currentLang = this.trans.currentLang
+  constructor(protected trans: TranslateService, private ls: LocalStorageService) {
+    this.currentLang = ls.getItem('lang') || this.trans.currentLang
     console.log('currentLang', this.currentLang)
   }
 
   onLanguageChange(event: SelectButtonOptionClickEvent) {
     console.log('onLanguageChange', event)
-    this.trans.use((event.option as LanguageEntry).code)
+    const code = (event.option as LanguageEntry).code
+    console.log('lang code', code)
+    this.trans.use(code)
+    this.ls.setItem('lang', code)
   }
 }
