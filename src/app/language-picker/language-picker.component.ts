d9a2b7ff5b80e658ee8e8bdba63b8f93b03f6c82
Tuan Minh Phan <phan.minh@tu-dortmund.de>
2025-03-06 21:15:01 +0100

    add language picker panel and functionality

---
+++ src/app/language-picker/language-picker.component.ts
@@ -0,0 +1,35 @@
+import {Component, Input} from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
+import {SelectButton, SelectButtonOptionClickEvent} from 'primeng/selectbutton';
+import {FormsModule} from '@angular/forms';
+
+export interface LanguageEntry {
+  text: string,
+  code: string
+}
+
+@Component({
+  selector: 'p-language-picker',
+  imports: [
+    SelectButton,
+    FormsModule
+  ],
+  templateUrl: './language-picker.component.html',
+  styleUrl: './language-picker.component.scss'
+})
+export class LanguagePickerComponent {
+  availableLangs: LanguageEntry[] = [{text: 'English', code: 'en'}, {text: 'Deutsch', code: 'de'}, {text: 'Tiếng Việt', code: 'vi'}];
+  currentLang: string;
+
+  @Input({required: true}) panelSize: 'small' | 'medium' | 'large' = 'medium'
+
+  constructor(protected trans: TranslateService) {
+    this.currentLang = this.trans.currentLang
+    console.log('currentLang', this.currentLang)
+  }
+
+  onLanguageChange(event: SelectButtonOptionClickEvent) {
+    console.log('onLanguageChange', event)
+    this.trans.use((event.option as LanguageEntry).code)
+  }
+}
