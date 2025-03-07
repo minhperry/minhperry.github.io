069feb042b3ab3a296601ea66b0466a0c9af545a
Tuan Minh Phan <phan.minh@tu-dortmund.de>
2025-03-07 18:34:36 +0100

    translate edu, jobs and timeline

--- src/app/experience/timeline/timeline.component.ts
+++ src/app/experience/timeline/timeline.component.ts
@@ -1,10 +1,13 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {TimelineEvent} from "../../../interfaces/date-entry";
 import {HttpClient} from "@angular/common/http";
+import {TranslatePipe, TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'p-timeline',
-  imports: [],
+  imports: [
+    TranslatePipe
+  ],
   templateUrl: './timeline.component.html',
   styleUrl: './timeline.component.scss'
 })
@@ -15,7 +18,7 @@ export class TimelineComponent implements OnInit{
   timeline: TimelineEvent[] = [];
   isLoaded = false;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private trans: TranslateService) {}
 
   ngOnInit() {
     this.http.get<TimelineEvent[]>(this.timelineDataFile).subscribe(data => {
@@ -25,8 +28,18 @@ export class TimelineComponent implements OnInit{
   }
 
   appendDate(event: TimelineEvent) {
-    return `FROM ${event.start} TO ${event.end ?? 'present'}`;
+    return `${this.translate('from')} ${event.start} ${this.translate('to')} ${event.end ?? this.translate('present') }`;
   }
+
+  private translations: Record<string, Record<string, string>> = {
+    from: { de: 'VON', en: 'FROM', vi: 'TỪ' },
+    to: { de: 'BIS', en: 'TO', vi: 'ĐẾN' },
+    present: { de: 'GEGENWÄRTIG', en: 'PRESENT', vi: 'HIỆN TẠI' }
+  };
+
+  private translate= (key: 'from' | 'to' | 'present') => {
+    return this.translations[key][this.trans.currentLang] || this.translations[key]['en'];
+  };
 
   protected readonly Array = Array;
 }
