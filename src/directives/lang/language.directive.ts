b5e25c8fa6fcf815faff8ef1aa14c2588a964056
Tuan Minh Phan <phan.minh@tu-dortmund.de>
2025-04-11 20:09:18 +0200

    rename language directive

--- src/directives/lang/language.directive.ts
+++ src/directives/lang/language.directive.ts
@@ -2,7 +2,7 @@ import {Directive, OnInit} from '@angular/core';
 import {LevelComponent} from "../../app/main-view/skills/level/level.component";
 
 @Directive({
-  selector: '[pLanguage]'
+  selector: '[ppLang]'
 })
 export class LanguageDirective implements OnInit {
 
