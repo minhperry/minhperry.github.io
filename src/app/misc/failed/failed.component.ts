d77c7ff806dede5326538560897fe61cba7fcbcc
Tuan Minh Phan <phan.minh@tu-dortmund.de>
2025-04-11 14:35:23 +0200

    extract loading and error to own component

---
+++ src/app/misc/failed/failed.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'p-failed',
+  imports: [],
+  templateUrl: './failed.component.html',
+  styleUrl: './failed.component.scss'
+})
+export class FailedComponent {
+
+}
