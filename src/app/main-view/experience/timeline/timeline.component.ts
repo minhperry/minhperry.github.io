41d30e593839cc1eda60baad713b3ec6c0a5b82d
Tuan Minh Phan <phan.minh@tu-dortmund.de>
2025-04-11 14:34:57 +0200

    finalized timeline success rewrite

--- src/app/main-view/experience/timeline/timeline.component.ts
+++ src/app/main-view/experience/timeline/timeline.component.ts
@@ -1,10 +1,17 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {TimelineEvent} from "../../../../interfaces/date-entry";
 import {HttpClient} from "@angular/common/http";
+import {injectQuery} from '@tanstack/angular-query-experimental';
+import {lastValueFrom} from 'rxjs';
+import {Timeline} from 'primeng/timeline';
+import {NgStyle} from '@angular/common';
 
 @Component({
   selector: 'pp-timeline',
-  imports: [],
+  imports: [
+    Timeline,
+    NgStyle
+  ],
   templateUrl: './timeline.component.html',
   styleUrl: './timeline.component.scss'
 })
@@ -12,21 +19,35 @@ export class TimelineComponent implements OnInit{
   @Input({required: true}) timelineDataFile!: string;
   @Input({required: true}) title = '';
 
-  timeline: TimelineEvent[] = [];
+  timeline__: TimelineEvent[] = [];
   isLoaded = false;
 
   constructor(private http: HttpClient) {}
 
+  readonly timeline = injectQuery(() => ({
+      enabled: this.timelineDataFile !== '',
+      queryKey: ['timeline', this.timelineDataFile],
+      queryFn: () =>lastValueFrom(
+        this.http.get<TimelineEvent[]>(this.timelineDataFile)
+      )
+    })
+  )
+
   ngOnInit() {
     this.http.get<TimelineEvent[]>(this.timelineDataFile).subscribe(data => {
-      this.timeline = data;
+      this.timeline__ = data;
     });
     this.isLoaded = true;
   }
 
   appendDate(event: TimelineEvent) {
-    return `FROM ${event.start} TO ${event.end ?? 'present'}`;
+    return `${event.start} - ${event.end ?? 'PRESENT'}`;
   }
 
   protected readonly Array = Array;
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  castToTimelineEvent = (event: any) => {
+    return event as TimelineEvent;
+  }
 }
