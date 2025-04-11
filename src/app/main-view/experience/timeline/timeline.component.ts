80fae2eb9805f3666eadb82d3dbebb1a803f853f
Tuan Minh Phan <phan.minh@tu-dortmund.de>
2025-04-11 15:07:33 +0200

    reused loading and failed

--- src/app/main-view/experience/timeline/timeline.component.ts
+++ src/app/main-view/experience/timeline/timeline.component.ts
@@ -1,26 +1,27 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {TimelineEvent} from "../../../../interfaces/date-entry";
 import {HttpClient} from "@angular/common/http";
 import {injectQuery} from '@tanstack/angular-query-experimental';
 import {lastValueFrom} from 'rxjs';
 import {Timeline} from 'primeng/timeline';
 import {NgStyle} from '@angular/common';
+import {FailedComponent} from '../../../misc/failed/failed.component';
+import {LoadingComponent} from '../../../misc/loading/loading.component';
 
 @Component({
   selector: 'pp-timeline',
   imports: [
     Timeline,
-    NgStyle
+    NgStyle,
+    FailedComponent,
+    LoadingComponent
   ],
   templateUrl: './timeline.component.html',
   styleUrl: './timeline.component.scss'
 })
-export class TimelineComponent implements OnInit{
+export class TimelineComponent {
   @Input({required: true}) timelineDataFile!: string;
   @Input({required: true}) title = '';
-
-  timeline__: TimelineEvent[] = [];
-  isLoaded = false;
 
   constructor(private http: HttpClient) {}
 
@@ -33,18 +34,9 @@ export class TimelineComponent implements OnInit{
     })
   )
 
-  ngOnInit() {
-    this.http.get<TimelineEvent[]>(this.timelineDataFile).subscribe(data => {
-      this.timeline__ = data;
-    });
-    this.isLoaded = true;
-  }
-
   appendDate(event: TimelineEvent) {
     return `${event.start} - ${event.end ?? 'PRESENT'}`;
   }
-
-  protected readonly Array = Array;
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   castToTimelineEvent = (event: any) => {
