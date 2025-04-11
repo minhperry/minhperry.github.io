bafe90dd9c69f2840828a5beb1fbbe1e56a914a2
Tuan Minh Phan <phan.minh@tu-dortmund.de>
2025-04-11 16:46:52 +0200

    add swipe animation to route changing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,16 @@ import {Component} from '@angular/core';
 import {Router, RouterOutlet} from '@angular/router';
 import {NavibarComponent} from "./navibar/navibar.component";
 import {FooterComponent} from "./footer/footer.component";
+import {swipe} from './animation/swipe';
 
 @Component({
   selector: 'pp-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [NavibarComponent, RouterOutlet, FooterComponent]
+  imports: [NavibarComponent, RouterOutlet, FooterComponent],
+  animations: [
+    swipe
+  ]
 })
 export class AppComponent {
   constructor(private router: Router) {
@@ -16,5 +20,9 @@ export class AppComponent {
   shouldRenderFooter(): boolean {
     const excluded = ['/']
     return !excluded.includes(this.router.url);
+  }
+
+  getRouterOutletState(outlet: RouterOutlet) {
+    return outlet.isActivated ? outlet.activatedRoute : '';
   }
 }
